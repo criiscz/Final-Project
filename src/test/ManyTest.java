7b2a0216e53aeb76b323a08c455fe4438ebd0f86
Cristian <criis@correo.etc>
2020-01-30 21:59:11 -0500

    Se termina de crear documentacion de DEPARTMENT

---
+++ src/test/ManyTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ManyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
